9cfe7a29822aae163857c691f2191cb2e3acbbb7
Tobias Schubotz <tobias.schubotz@gmail.com>
2022-09-19 22:06:31 +0200

    Updated csvs via #294

--- review/check.py
+++ review/check.py
@@ -1,10 +1,13 @@
 # Script to check if safes in current.csv still have an allocation in safe_user_allocations_reworked.csv
 
 import pandas as pd
+import sys
 
 # Load current potential airdrop farming safes.
 current = pd.read_csv('current.csv', index_col=0)
 current.index = current.index.str.lower()
+
+PRINT_ALLOCATIONS = False if len(sys.argv) == 1 or sys.argv[1] != 'a' else True
 
 # Load current allocations
 allocations = pd.read_csv('../safe_user_allocations_reworked.csv', index_col=0)
@@ -19,7 +22,11 @@ available = []
 current.reset_index()
 for index, row in current.iterrows():
     if index in allocations.index:
-        print(index)
+        if PRINT_ALLOCATIONS:
+            tokens = allocations.loc[index]['tokens']
+            print('{},{}'.format(index, tokens))
+        else:
+            print(index)
         available.append(index)
     else:
         already_removed.append(index)
