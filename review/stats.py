7504511a086587cc38e3d6e6652e6de357cfff09
Tobias Schubotz <tobias.schubotz@gmail.com>
2022-09-22 12:19:55 +0200

    Fix stats script

--- review/stats.py
+++ review/stats.py
@@ -26,7 +26,7 @@ for index, row in reports.iterrows():
 safe_per_reward_address_list = [(k, v) for k, v in safe_per_reward_address_dict.items()]
 
 safe_per_reward_address = pd.DataFrame(safe_per_reward_address_list, columns=['safe_address', 'safe_saved'])
-safe_per_reward_address.set_index('safe_address')
+safe_per_reward_address = safe_per_reward_address.set_index('safe_address')
 safe_per_reward_address['safe_reward'] = safe_per_reward_address['safe_saved'] / 4
 safe_per_reward_address = safe_per_reward_address.sort_values('safe_reward', ascending=False)
 safe_per_reward_address.to_csv('stats_safe_per_reward_address.csv', sep=',')
@@ -36,4 +36,3 @@ print('{} SAFE saved'.format(safe_per_reward_address.sum()['safe_saved']))
 print('{} Safes removed'.format(reports.count()[0]))
 print('{} SAFE rewarded'.format(safe_per_reward_address.sum()['safe_reward']))
 print('{} Safes rewarded'.format(safe_per_reward_address.count()[0]))
-import pdb; pdb.set_trace()
